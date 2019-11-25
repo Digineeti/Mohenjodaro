eb0c810178e96600f612269d460ddaaf6bbeb061
vikash <vikash.harshitmeet@gmailcom>
2019-11-25 18:28:34 +0530

    dialogue

--- Mohanjo daro/Assets/Script/Dialogue/DialogueMaster.cs
+++ Mohanjo daro/Assets/Script/Dialogue/DialogueMaster.cs
@@ -2,38 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 [SerializeField]
 public class DialogueMaster:MonoBehaviour
 {
-    public Text nameText;
-    public Text dialogueText;
-
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
     public Animator animator;
+    public GameObject Dialoguebox;
 
     private Queue<string> sentences;
+    private Queue<string> names;
 
     // Use this for initialization
     void Start()
     {
         //animator.SetBool("IsOpen", false);
-        sentences = new Queue<string>();
+        //Dialoguebox.SetActive(false);
+        sentences = new Queue<string>(); names = new Queue<string>();
     }
-
+    int activeDialogue = 0;
     public void StartDialogue(Dialogue dialogue)
     {
-        animator.SetBool("IsOpen", true);
-
-        nameText.text = dialogue.name;
-
+        //animator.SetBool("IsOpen", true);
+        activeDialogue = 1;
+      
+        
         sentences.Clear();
-
         foreach (string sentence in dialogue.sentences)
         {
-            sentences.Enqueue(sentence);
-        }
+            names.Enqueue(sentence);
+            int index = sentence.ToString().IndexOf(":");
+            //nameText.text = sentence.ToString().Substring(0, index);           
+            sentences.Enqueue(sentence.ToString().Substring(index + 1, sentence.ToString().Length - (index + 1)));
 
+            //dialogueText.text = sentence.ToString().Substring(index + 1, sentence.ToString().Length - (index + 1));
+        }
         DisplayNextSentence();
+    }
+    private void Update()
+    {
+        if(activeDialogue>0)
+        {
+            Dialoguebox.SetActive(true);
+        }
+        else
+        {
+            Dialoguebox.SetActive(false);
+        }
     }
 
     public void DisplayNextSentence()
@@ -43,8 +61,10 @@ public class DialogueMaster:MonoBehaviour
             EndDialogue();
             return;
         }
-
-        string sentence = sentences.Dequeue();
+        string sentence = sentences.Dequeue(); 
+        string Name= names.Dequeue();
+        int index = Name.ToString().IndexOf(":");
+        nameText.text = Name.ToString().Substring(0, index);
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
@@ -61,6 +81,8 @@ public class DialogueMaster:MonoBehaviour
 
     void EndDialogue()
     {
-        animator.SetBool("IsOpen", false);
+        //animator.SetBool("IsOpen", false);
+        activeDialogue = 0;
+        Dialoguebox.SetActive(false);
     }
 }
