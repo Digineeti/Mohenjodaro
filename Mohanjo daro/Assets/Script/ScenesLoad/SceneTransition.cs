9d026443b58608d4b7e8df5d92e632b17b561455
vikash <vikash.harshitmeet@gmailcom>
2020-01-27 18:56:07 +0530

    scene transition

---
+++ Mohanjo daro/Assets/Script/ScenesLoad/SceneTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    //public string SceneToLoad;
+    public int SceneIndex;
+    public Animator transition;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "ScenePlayer")
+        {          
+            //SceneManager.LoadScene(SceneIndex);
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transition.SetTrigger("end");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    //AsyncOperation operation = SceneManager.LoadSceneAsync(Sceneindex);
+    //while (!operation.isDone)
+    //{
+    //    transition.SetTrigger("Start");
+    //    yield return null;
+    //    //SceneManager.LoadScene(Sceneindex);
+    //}
+}
