98f5d8382b29888a30c344fbbab64b26983e43a1
vikash <vikash.harshitmeet@gmailcom>
2020-02-10 18:37:08 +0530

    prop build in scene

---
+++ Mohanjo daro/Assets/Script/Player/LayerShorter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerShorter : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "ScenePlayer")
+        {
+            gameObject.transform.parent.GetComponent<SpriteRenderer>().sortingOrder = 100;
+
+           gameObject.transform.parent.GetComponent<SpriteRenderer>().color=new Color(1,1,1,0.6f);
+            //tmp.a = 0f;
+           //gameObject.transform.parent.GetComponent<SpriteRenderer>().color = tmp;
+
+            // gameObject.transform.parent.GetComponent<SpriteRenderer>().sortingOrder = collision.GetComponent<SpriteRenderer>().sortingOrder - 1;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Color tmp = gameObject.transform.parent.GetComponent<SpriteRenderer>().color;
+        tmp.a = 1f;
+        gameObject.transform.parent.GetComponent<SpriteRenderer>().color = tmp;
+
+    }
+}
---
+++ Mohanjo daro/Assets/Script/Player/LayershorterRoot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayershorterRoot : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "ScenePlayer")
+        {
+            gameObject.transform.parent.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        }
+    }
+}
