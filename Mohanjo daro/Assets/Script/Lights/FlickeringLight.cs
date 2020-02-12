685c8b159cf7c6800c9a27606c8e58f3fd93944c
vikash <vikash.harshitmeet@gmailcom>
2020-02-12 18:10:49 +0530

    2d light

---
+++ Mohanjo daro/Assets/Script/Lights/FlickeringLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FlickeringLight : MonoBehaviour
+{
+     public UnityEngine.Experimental.Rendering.LWRP.Light2D m_Light2D;
+    Light light;
+    public float mintime;
+    public float maxtime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //light = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+
+    IEnumerator Flashing()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(mintime,maxtime));
+            m_Light2D.enabled = !m_Light2D.enabled;
+        }
+    }
+
+   
+}
