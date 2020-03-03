20462edc3c4e87540315ad24c2bc79c60011f0ff
vikash <vikash.harshitmeet@gmailcom>
2020-03-03 18:50:30 +0530

    house interior

---
+++ Mohanjo daro/Assets/Script/ScenesLoad/LoadInterior/HouseEnter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class HouseEnter : MonoBehaviour
+{
+
+    public Tilemap ExteriorHouse;
+    public Tilemap ExteriorProps;
+
+    public Tilemap InteriorHouse;
+    public Tilemap InteriorProps;
+    private Animator transition;         //camera transition effect.
+    bool check;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ScenePlayer")
+        {
+
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transition = GameObject.Find("CinemachineCamera").GetComponent<Animator>();
+        transition.SetTrigger("end");
+
+        //SceneManager.LoadScene(TransitionScene);
+        yield return new WaitForSeconds(0.5f);
+        check = !check;
+        if (check)
+        {
+            //collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 200;
+            ExteriorHouse.color = new Color(1, 1, 1, 0.1f);
+            ExteriorProps.color = new Color(1, 1, 1, 0.1f);
+            InteriorHouse.gameObject.SetActive(true);
+            InteriorProps.gameObject.SetActive(true);
+        }
+        else
+        {
+            //collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 200;
+            ExteriorHouse.color = new Color(1, 1, 1, 1f);
+            ExteriorProps.color = new Color(1, 1, 1, 1f);
+            InteriorHouse.gameObject.SetActive(false);
+            InteriorProps.gameObject.SetActive(false);
+        }
+       
+        transition.SetTrigger("start");
+    }
+}
