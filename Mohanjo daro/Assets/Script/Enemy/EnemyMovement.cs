794cf33414c2eea03ca7eabe400fa91d2f1868e7
vikash <vikash.harshitmeet@gmailcom>
2020-06-12 14:13:19 +0530

    dialogue in prologue

--- Mohanjo daro/Assets/Script/Enemy/EnemyMovement.cs
+++ Mohanjo daro/Assets/Script/Enemy/EnemyMovement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-
+    #region movement_variable
     public GameObject enemy;
     public GameObject midPoint;
-    public Rigidbody2D RD;
+    private Rigidbody2D RD;
     private Animator amin;
     private bool flip;
+    #endregion
 
+    #region enemy_ineract_payer
+    //public float look_Radius;
+    //public Transform traget;
+
+    #endregion
+
+    //
     //amin.SetFloat("MoveX", input.horizontal);
     //amin.SetFloat("MoveY", input.vertical);
     //amin.SetBool("PlayerMoving", playerMoving);
@@ -26,47 +34,59 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(enemy.transform.position.x <= midPoint.transform.position.x+5 && flip)
-        {
-          
-                RD.velocity = new Vector2(3f, 0f);
-                amin.SetFloat("MoveX", 3f);
-                //movement 
-           
-            //move in positive direction
-        }
-        else
-        {
-            if(flip==true)
-            {
-                enemy.transform.localScale.Scale(new Vector3(-1,0,0));
-            }
-            flip = false;
-            if(enemy.transform.position.x <= midPoint.transform.position.x - 5 )
-            {
-                flip = true;
-                enemy.transform.localScale.Scale(new Vector3(-1, 0, 0));
-            }
-
-            
-            RD.velocity = new Vector2(-3f, 0f);
-            amin.SetFloat("MoveX", -3f);
-            //call negative mov
-
-        }
-        //else if (enemy.transform.position.x > midPoint.transform.position.x - 10)
+        #region enemy_Horiontal_Movement
+        //if (enemy.transform.position.x <= midPoint.transform.position.x + 5 && flip)
         //{
-        //    Debug.Log("Negative");
-        //    if (enemy.transform.position.x <= midPoint.transform.position.x)
+
+        //    RD.velocity = new Vector2(3f, 0f);
+        //    amin.SetFloat("MoveX", 3f);
+        //    //movement 
+
+        //    //move in positive direction
+        //}
+        //else
+        //{
+        //    if (flip == true)
         //    {
-        //        //flip()
+        //        enemy.transform.localScale.Scale(new Vector3(-1, 0, 0));
         //    }
-        //    else
+        //    flip = false;
+        //    if (enemy.transform.position.x <= midPoint.transform.position.x - 5)
         //    {
-        //        //movement 
+        //        flip = true;
+        //        enemy.transform.localScale.Scale(new Vector3(-1, 0, 0));
+        //    }
+
+        //    RD.velocity = new Vector2(-3f, 0f);
+        //    amin.SetFloat("MoveX", -3f);
+        //    //call negative mov
+
+        //}
+
+        #endregion
+        #region move_toward_to_player
+        //float distance = Vector2.Distance(traget.position,transform.position);
+        //if(distance<look_Radius)
+        //{
+
+        //        RD.velocity = new Vector2(0f, -3f);
+        //    amin.SetBool("EnemyMoving", true);
+        //    amin.SetFloat("MoveY", -3f);
+        //    if(distance<1)
+        //    {
+        //        RD.velocity = new Vector2(0f, 0f);
+        //        amin.SetFloat("LastMoveY", -1f);
+        //        amin.SetBool("EnemyMoving", false);
+        //        Globalvariable.Dialogue_Open = true;
         //    }
         //}
-        //positive motion
+        #endregion
+
     }
-   
+
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(transform.position, look_Radius);
+    //}
 }
