a9fe373658a4ba8270887ec8ccd7413f77801059
vikash <vikash.harshitmeet@gmailcom>
2020-06-18 16:17:08 +0530

    multiple save file

--- Mohanjo daro/Assets/Script/Saving/SaveSystem.cs
+++ Mohanjo daro/Assets/Script/Saving/SaveSystem.cs
@@ -16,15 +16,45 @@ public static class SaveSystem
     }
     public static void save(string savestring)
     {
-        File.WriteAllText(SAVE_FOLDER + "/save.txt", savestring); 
+        int savenumber = 1;
+        while(File.Exists(SAVE_FOLDER +"save_" +savenumber+ ".txt"))
+        { savenumber++; }
+        File.WriteAllText(SAVE_FOLDER + "save_"+ savenumber+ ".txt", savestring); 
     }
     public static string load()
     {
-        if (File.Exists(SAVE_FOLDER + "/save.txt")){
-            string savestring = File.ReadAllText(SAVE_FOLDER + "/save.txt");
-            return savestring;
-        }else{
-            return null;
+
+        DirectoryInfo directoryInfo=new DirectoryInfo(SAVE_FOLDER);
+        FileInfo[] savefile = directoryInfo.GetFiles();
+        FileInfo mostrecentfile = null;
+        foreach(FileInfo files in savefile)
+        {
+            if (mostrecentfile == null)
+            {
+                mostrecentfile = files;
+            }
+            else
+            {
+                if (files.LastWriteTime > mostrecentfile.LastWriteTime)
+                {
+                    mostrecentfile = files;
+                }
+            }
+          
         }
+
+        if(mostrecentfile!=null)
+        {
+            string savestring = File.ReadAllText(mostrecentfile.FullName);
+            return savestring;
+        }
+        else { return null; }
+
+        //if (File.Exists(SAVE_FOLDER + "save.txt")){
+        //    string savestring = File.ReadAllText(SAVE_FOLDER + "save.txt");
+        //    return savestring;
+        //}else{
+        //    return null;
+        //}
     }
 }
