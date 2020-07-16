1a806d36b64cfaefd189cbcae57ca13f4fc3440b
vikash <vikash.harshitmeet@gmailcom>
2020-07-16 14:37:05 +0530

    boat

---
+++ Mohanjo daro/Assets/Script/Props/Boat_Motion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat_Motion : MonoBehaviour
+{
+
+    Rigidbody2D RD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RD = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RD.velocity = new Vector2(0f, 1.5f);
+    }
+}
